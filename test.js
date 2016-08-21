f396815b65cdf60634eced2f80786c31db3c3ee9
lejoe <ciao@lejoe.com>
2016-08-21 22:11:30 +0200

    Makes the test more readable

--- test.js
+++ test.js
@@ -3,55 +3,53 @@ import difflet from 'difflet';
 import m from './';
 
 const diff = difflet({indent: 2});
+const expected = {
+	'elements/colors': {
+		style: true,
+		documentation: true
+	},
+	'elements/grid': {
+		style: true,
+		documentation: true
+	},
+	'modules/bar': {
+		template: true,
+		script: true,
+		definition: {
+			data: {
+				arg1: 'ads',
+				arg2: 'asd'
+			},
+			options: {
+				opt1: ['op1-defaul', 'op1-other'],
+				opt2: ['op2-defaul', 'op2-other']
+			}
+		},
+		documentation: true,
+		style: true
+	},
+	'modules/foo': {
+		template: true,
+		script: true,
+		definition: {
+			data: {
+				arg1: 'ads',
+				arg2: 'asd'
+			},
+			options: {
+				opt1: ['op1-defaul', 'op1-other'],
+				opt2: ['op2-defaul', 'op2-other']
+			}
+		},
+		documentation: true,
+		style: true
+	}
+	// ,
+	// 'pages/index.jade': {
+	// }
+};
 
-test.cb(t => {
-	const expected = {
-		'elements/colors': {
-			style: true,
-			documentation: true
-		},
-		'elements/grid': {
-			style: true,
-			documentation: true
-		},
-		'modules/bar': {
-			template: true,
-			script: true,
-			definition: {
-				data: {
-					arg1: 'ads',
-					arg2: 'asd'
-				},
-				options: {
-					opt1: ['op1-defaul', 'op1-other'],
-					opt2: ['op2-defaul', 'op2-other']
-				}
-			},
-			documentation: true,
-			style: true
-		},
-		'modules/foo': {
-			template: true,
-			script: true,
-			definition: {
-				data: {
-					arg1: 'ads',
-					arg2: 'asd'
-				},
-				options: {
-					opt1: ['op1-defaul', 'op1-other'],
-					opt2: ['op2-defaul', 'op2-other']
-				}
-			},
-			documentation: true,
-			style: true
-		}
-		// ,
-		// 'pages/index.jade': {
-		// }
-	};
-	m('./fixtures/simple').then(actual => {
-		t.deepEqual(actual, expected, diff.compare(actual, expected));
-		t.end();
-	});
+test('Tests the normal use case', async t => {
+	const actual = await m('./fixtures/simple');
+	t.deepEqual(actual, expected, diff.compare(actual, expected));
 });
