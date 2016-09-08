05c74b8d9f51a763c360861106829eef08cb9f2c
João Figueiredo <lucalanca2@hotmail.com>
2016-09-08 10:58:10 +0200

    Refactor to make the module parser immutable

--- module-parser.js
+++ module-parser.js
@@ -11,31 +11,51 @@ function trimObject(json) {
 	return json;
 }
 
+const FILE_TYPES = [
+	'template', 'definition', 'documentation',
+	'script', 'style'
+];
+
+const PARSER_BY_FILE_TYPE = {
+	template: moduleHelper.parseTemplate,
+	definition: moduleHelper.parseDefinition,
+	documentation: moduleHelper.parseDocumentation,
+	script: moduleHelper.parseScript,
+	style: moduleHelper.parseStyle
+};
+
 module.exports = (modulePath, cwd) => {
 	const name = moduleHelper.extractName(modulePath);
 	const group = moduleHelper.extractGroup(modulePath);
 
-	const moduleJSON = {
-		template: moduleHelper.parseTemplate,
-		definition: moduleHelper.parseDefinition,
-		documentation: moduleHelper.parseDocumentation,
-		script: moduleHelper.parseScript,
-		style: moduleHelper.parseStyle
-	};
+	const filetypeParsersPromise = FILE_TYPES
+		.map(filetype => PARSER_BY_FILE_TYPE[filetype](modulePath, cwd));
 
-	const attibutesPromises = Object.keys(moduleJSON).map(key => {
-		return moduleJSON[key](modulePath, cwd).then(value => {
-			moduleJSON[key] = value;
+	// wait for all promise of all filetypes to be completed
+	return Promise.all(filetypeParsersPromise)
+		// convert them to an object
+		.then(attributesResult => {
+			return FILE_TYPES.reduce((acc, curr, currIdx) => {
+				return Object.assign(
+					{},
+					acc,
+					{[curr]: attributesResult[currIdx]}
+				);
+			}, {});
+		})
+		// add path
+		.then(moduleJson => {
+			return Object.assign(
+				{},
+				moduleJson,
+				{path: moduleHelper.buildPath(modulePath, cwd)}
+			);
+		})
+		.then(moduleJson => {
+			return {
+				[group]: {
+					[name]: trimObject(moduleJson)
+				}
+			};
 		});
-	});
-
-	return Promise.all(attibutesPromises).then(() => {
-		moduleJSON.path = moduleHelper.buildPath(modulePath, cwd);
-
-		return {
-			[group]: {
-				[name]: trimObject(moduleJSON)
-			}
-		};
-	});
 };
